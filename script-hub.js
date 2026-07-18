c92708460a518dee8d3f54fe28e619676986f886
Biepoe <50437909+Biepoe@users.noreply.github.com>
2026-07-18 22:17:43 +0700

    Update script-hub.js

--- script-hub.js
+++ script-hub.js
@@ -1,1 +1,8 @@
+// =========================================================
+// SCRIPT UNTUK HUB
+// =========================================================
 
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('✅ Halaman dimuat, script.js berjalan.');
+
+    const BACKEND_URL = 'https://dashboard-dpp-backend.onrender.com';
